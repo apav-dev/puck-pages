d40c647715cbd76ea2a1a7de4f691af3c9c922c1
apav-dev <apavlick@yext.com>
2023-11-30 20:04:34 -0500

    Remove unused code and update getPath function

--- src/templates/location.tsx
+++ src/templates/location.tsx
@@ -1,14 +1,22 @@
-import { Template, TemplateRenderProps } from "@yext/pages";
+import {
+  GetPath,
+  Template,
+  TemplateProps,
+  TemplateRenderProps,
+} from "@yext/pages";
 import "../index.css";
 import { Locations as LocationsType } from "../types/autogen";
 import LocationLayout from "../layouts/location/template";
-import test from "../../stream.json";
+import streamConfig from "../../stream.json";
 
-export { getPath } from "../layouts/location/getPath";
 export { transformProps } from "../layouts/location/transformProps";
 export { getHeadConfig } from "../layouts/location/getHeadConfig";
 
-export const config = test;
+export const config = streamConfig;
+
+export const getPath: GetPath<TemplateProps> = ({ document }) => {
+  return document.slug ?? document.name;
+};
 
 // Initialize the configuration asynchronously
 const Location: Template<TemplateRenderProps<LocationsType>> = (data) => {
