27d601c569d080588f6e9610f4bdaeedc75c61cd
apav-dev <apavlick@yext.com>
2024-03-13 13:18:26 -0400

    moving everything to location.tsx

--- src/templates/location.tsx
+++ src/templates/location.tsx
@@ -1,17 +1,16 @@
 import {
-  GetPath,
+  GetHeadConfig,
+  HeadConfig,
   Template,
   TemplateConfig,
-  TemplateProps,
   TemplateRenderProps,
 } from "@yext/pages";
 import "../index.css";
 import { Locations as LocationsType } from "../types/autogen";
-import LocationLayout from "../layouts/location/template";
-import streamConfig from "../../stream.json";
-
-export { transformProps } from "../layouts/location/transformProps";
-export { getHeadConfig } from "../layouts/location/getHeadConfig";
+import { Render } from "@measured/puck";
+import { Config, Data, resolveAllData } from "@measured/puck";
+import puckConfig, { Props } from "../config";
+import { RootProps } from "../config/root";
 
 export const config: TemplateConfig = {
   stream: {
@@ -40,13 +39,73 @@ export const config: TemplateConfig = {
   },
 };
 
-export const getPath: GetPath<TemplateProps> = ({ document }) => {
-  return document.slug ?? document.name;
+function addDocumentPropsToTemplateData<T>(
+  config: Config<Props, RootProps>,
+  templateData: Data,
+  document: T
+) {
+  templateData.content.forEach((component) => {
+    const componentConfig = config.components[component.type];
+    if (componentConfig && componentConfig.fields["document"]) {
+      // If the document field is defined in the config, add it to the component props
+      component.props["document"] = document;
+    }
+  });
+}
+
+export const transformProps = async (
+  data: TemplateRenderProps<LocationsType>
+) => {
+  const { document } = data;
+
+  if (!document.c_linkedTemplate?.[0].c_template?.url) {
+    return data;
+  }
+
+  const jsonUrl = document.c_linkedTemplate?.[0].c_template?.url;
+  const response = await fetch(jsonUrl);
+  let templateData: Data = await response.json();
+
+  addDocumentPropsToTemplateData<LocationsType>(conf, templateData, document);
+
+  templateData = await resolveAllData(templateData, conf);
+
+  // console.log("templateData", JSON.stringify(templateData, null, 2));
+
+  return {
+    ...data,
+    document: {
+      ...document,
+      templateData,
+    },
+  };
 };
 
-// Initialize the configuration asynchronously
+export const getHeadConfig: GetHeadConfig<TemplateRenderProps> = ({
+  relativePrefixToRoot,
+  path,
+  document,
+}): HeadConfig => {
+  return {
+    title: document.name,
+    charset: "UTF-8",
+    viewport: "width=device-width, initial-scale=1",
+    tags: [
+      {
+        type: "link",
+        attributes: {
+          rel: "icon",
+          type: "image/x-icon",
+          href: "/yext-favicon.ico",
+        },
+      },
+    ],
+  };
+};
+
 const Location: Template<TemplateRenderProps<LocationsType>> = (data) => {
-  return <LocationLayout {...data} />;
+  const { templateData } = data.document;
+  return <Render config={puckConfig} data={templateData} />;
 };
 
 export default Location;
