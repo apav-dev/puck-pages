3dd5e2ea5d315e7dcfa30269819e1ec0caa34bf1
apav-dev <apavlick@yext.com>
2024-03-14 18:58:32 -0400

    update stream

--- src/templates/location.tsx
+++ src/templates/location.tsx
@@ -8,9 +8,7 @@ import {
 import "../index.css";
 import { Locations as LocationsType } from "../types/autogen";
 import { Render } from "@measured/puck";
-import { Config, Data, resolveAllData } from "@measured/puck";
-import puckConfig, { Props } from "../config";
-import { RootProps } from "../config/root";
+import puckConfig from "../config";
 
 export const config: TemplateConfig = {
   stream: {
@@ -31,60 +29,42 @@ export const config: TemplateConfig = {
       "c_coverPhoto",
       "c_linkedTemplate.id",
       "c_linkedTemplate.name",
-      "c_linkedTemplate.c_templateJson",
       "c_linkedTemplate.c_linkedEntities.id",
-      "c_template",
+      "c_linkedTemplate.c_template",
     ],
     filter: { entityIds: ["aarons-store", "lucs-store"] },
     localization: { locales: ["en"] },
   },
 };
 
-// function addDocumentPropsToTemplateData<T>(
-//   config: Config<Props, RootProps>,
-//   templateData: Data,
-//   document: T
-// ) {
-//   templateData.content.forEach((component) => {
-//     const componentConfig = config.components[component.type];
-//     if (componentConfig && componentConfig.fields["document"]) {
-//       // If the document field is defined in the config, add it to the component props
-//       component.props["document"] = document;
-//     }
-//   });
-// }
+export const transformProps = async (
+  data: TemplateRenderProps<LocationsType>
+) => {
+  const { document } = data;
 
-// export const transformProps = async (
-//   data: TemplateRenderProps<LocationsType>
-// ) => {
-//   const { document } = data;
+  const linkedTemplateEntity = document.c_linkedTemplate?.[0];
 
-//   if (!document.c_linkedTemplate?.[0].c_template?.url) {
-//     return data;
-//   }
+  if (!linkedTemplateEntity && !linkedTemplateEntity.c_template) {
+    return data;
+  }
 
-//   const jsonUrl = document.c_linkedTemplate?.[0].c_template?.url;
-//   const response = await fetch(jsonUrl);
-//   let templateData: Data = await response.json();
-
-//   addDocumentPropsToTemplateData<LocationsType>(conf, templateData, document);
-
-//   templateData = await resolveAllData(templateData, conf);
-
-//   // console.log("templateData", JSON.stringify(templateData, null, 2));
-
-//   return {
-//     ...data,
-//     document: {
-//       ...document,
-//       templateData,
-//     },
-//   };
-// };
+  try {
+    const visualTemplate = JSON.parse(linkedTemplateEntity.c_template);
+    console.log("Successfully parsed visualTemplate");
+    return {
+      ...data,
+      document: {
+        ...document,
+        visualTemplate,
+      },
+    };
+  } catch (e) {
+    console.error("Failed to parse visualTemplate" + e);
+    return data;
+  }
+};
 
 export const getHeadConfig: GetHeadConfig<TemplateRenderProps> = ({
-  relativePrefixToRoot,
-  path,
   document,
 }): HeadConfig => {
   return {
@@ -104,9 +84,15 @@ export const getHeadConfig: GetHeadConfig<TemplateRenderProps> = ({
   };
 };
 
+export const getPath = (data: TemplateRenderProps<LocationsType>) => {
+  const { document } = data;
+  return document.slug ? document.slug : document.id;
+};
+
 const Location: Template<TemplateRenderProps<LocationsType>> = (data) => {
-  const { c_template } = data.document;
-  return <Render config={puckConfig} data={c_template} />;
+  const { visualTemplate } = data.document;
+
+  return <Render config={puckConfig} data={visualTemplate} />;
 };
 
 export default Location;
