40baeafafd81dad38235b8257abef7dfd2148b9d
apav-dev <apavlick@yext.com>
2024-04-19 09:55:28 -0400

    Add FinancialPro template to src/templates/financial-pro.tsx

---
+++ src/templates/financial-pro.tsx
@@ -0,0 +1,97 @@
+import {
+  GetHeadConfig,
+  HeadConfig,
+  Template,
+  TemplateConfig,
+  TemplateRenderProps,
+} from "@yext/pages";
+import "../index.css";
+import { Locations as LocationsType } from "../types/autogen";
+import { Data, Render } from "@measured/puck";
+import puckConfig from "../config";
+import { DocumentProvider } from "../hooks/useDocument";
+
+export const config: TemplateConfig = {
+  stream: {
+    $id: "financial-pro",
+    fields: [
+      "id",
+      "name",
+      "slug",
+      // "c_coverPhoto",
+      "c_linkedTemplate.id",
+      "c_linkedTemplate.name",
+      "c_linkedTemplate.c_linkedEntities.id",
+      "c_linkedTemplate.c_template",
+      // component fields
+      // "c_hero",
+    ],
+    filter: { entityIds: ["andrew-sanchez"] },
+    localization: { locales: ["en"] },
+  },
+};
+
+export const transformProps = async (
+  data: TemplateRenderProps<LocationsType>
+) => {
+  const { document } = data;
+
+  const linkedTemplateEntity = document.c_linkedTemplate?.[0];
+
+  if (!linkedTemplateEntity || !linkedTemplateEntity.c_template) {
+    return data;
+  }
+
+  try {
+    const visualTemplate: Data = JSON.parse(linkedTemplateEntity.c_template);
+    console.log("Successfully parsed visualTemplate");
+    return {
+      ...data,
+      document: {
+        ...document,
+        visualTemplate,
+      },
+    };
+  } catch (e) {
+    console.error("Failed to parse visualTemplate" + e);
+    return data;
+  }
+};
+
+export const getHeadConfig: GetHeadConfig<TemplateRenderProps> = ({
+  document,
+}): HeadConfig => {
+  return {
+    title: document.name,
+    charset: "UTF-8",
+    viewport: "width=device-width, initial-scale=1",
+    tags: [
+      {
+        type: "link",
+        attributes: {
+          rel: "icon",
+          type: "image/x-icon",
+          href: "/yext-favicon.ico",
+        },
+      },
+    ],
+  };
+};
+
+export const getPath = (data: TemplateRenderProps<LocationsType>) => {
+  const { document } = data;
+  return document.slug ? document.slug : document.id;
+};
+
+const FinancialPro: Template<TemplateRenderProps<LocationsType>> = (data) => {
+  const { document } = data;
+  const { visualTemplate } = document;
+
+  return (
+    <DocumentProvider value={document}>
+      <Render config={puckConfig} data={visualTemplate} />
+    </DocumentProvider>
+  );
+};
+
+export default FinancialPro;
